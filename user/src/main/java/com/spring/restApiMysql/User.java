1fcfb0fd6baa780e6d8388a5e1ca30234e59b43b
satawatnack <60070093@kmitl.ac.th>
2019-10-15 17:57:25 +0700

    refactored user model

--- user/src/main/java/com/spring/restApiMysql/User.java
+++ user/src/main/java/com/spring/restApiMysql/User.java
@@ -1,4 +1,4 @@
-	package com.spring.restApiMysql;
+package com.spring.restApiMysql;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -6,7 +6,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-//@Table(name = "User") if your table name is different from the class name.
 public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
@@ -29,6 +28,7 @@ public class User {
 		this.tel = tel;
 		this.email = email;
 	}
+
 	public User(int id, String firstname, String middlename, String lastname, String tel, String email) {
 		this.id = id;
 		this.firstname = firstname;
@@ -37,39 +37,51 @@ public class User {
 		this.tel = tel;
 		this.email = email;
 	}
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getFirstname() {
 		return firstname;
 	}
+
 	public void setFirstname(String firstname) {
 		this.firstname = firstname;
 	}
+
 	public String getMiddlename() {
 		return middlename;
 	}
+
 	public void setMiddleName(String middlename) {
 		this.middlename = middlename;
 	}
+
 	public String getLastname() {
 		return lastname;
 	}
+
 	public void setLastname(String lastname) {
 		this.lastname = lastname;
 	}
+
 	public String getTel() {
 		return tel;
 	}
+
 	public void setTel(String tel) {
 		this.tel = tel;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+	
 	public void setEmail(String email) {
 		this.email = email;
 	}
