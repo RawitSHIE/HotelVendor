f5b8bd71a7293bbb275efadc1ba8ac45279e2a4a
RawitSHIE <Rawitgun@gmail.com>
2019-10-21 18:07:16 +0700

    added booking controller

---
+++ booking/src/main/java/sop/project/booking/BookingController.java
@@ -0,0 +1,13 @@
+package sop.project.booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingController {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookingController.class, args);
+    }
+
+}
