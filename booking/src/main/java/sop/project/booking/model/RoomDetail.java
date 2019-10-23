6a620f016630c14bfadde7838b90ca80bcaa8f77
RawitSHIE <Rawitgun@gmail.com>
2019-10-23 17:36:47 +0700

    updated room detail model

--- booking/src/main/java/sop/project/booking/model/RoomDetail.java
+++ booking/src/main/java/sop/project/booking/model/RoomDetail.java
@@ -1,11 +1,15 @@
 package sop.project.booking.model;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 
+@Entity
+@Table(name = "RoomDetail")
 public class RoomDetail {
+
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private long id;
@@ -16,12 +20,22 @@ public class RoomDetail {
     @NotNull
     private long price;
 
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "bookingId", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIgnore
+    private Booking booking;
+
     public long getId() {
         return id;
     }
 
     public String getRoomId() {
         return roomId;
+    }
+
+    public Booking getBooking() {
+        return booking;
     }
 
     public long getPrice() {
@@ -34,5 +48,9 @@ public class RoomDetail {
 
     public void setPrice(long price) {
         this.price = price;
+    }
+
+    public void setBooking(Booking booking) {
+        this.booking = booking;
     }
 }
