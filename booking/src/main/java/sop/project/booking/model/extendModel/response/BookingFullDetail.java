5b5de2e935f1b48edfec4ce254f9806e63c8702b
RawitSHIE <Rawitgun@gmail.com>
2019-10-27 23:36:40 +0700

    added booking full detail response format

---
+++ booking/src/main/java/sop/project/booking/model/extendModel/response/BookingFullDetail.java
@@ -0,0 +1,41 @@
+package sop.project.booking.model.extendModel.response;
+
+import sop.project.booking.model.Booking;
+import sop.project.booking.model.RoomTypeDetail;
+import sop.project.booking.model.extendModel.Hotel;
+import sop.project.booking.model.extendModel.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookingFullDetail {
+    Hotel hotel;
+    User user;
+    Booking booking;
+    List<RoomTypeDetail> roomTypeDetails = new ArrayList<RoomTypeDetail>();
+
+    public BookingFullDetail(){}
+
+    public BookingFullDetail(Hotel hotel, User user, Booking booking, List<RoomTypeDetail> roomTypeDetails) {
+        this.hotel = hotel;
+        this.user = user;
+        this.booking = booking;
+        this.roomTypeDetails = roomTypeDetails;
+    }
+
+    public void setHotel(Hotel hotel) {
+        this.hotel = hotel;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public void setBooking(Booking booking) {
+        this.booking = booking;
+    }
+
+    public void setRoomTypeDetails(List<RoomTypeDetail> roomTypeDetails) {
+        this.roomTypeDetails = roomTypeDetails;
+    }
+}
