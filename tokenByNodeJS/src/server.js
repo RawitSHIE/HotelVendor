98db433d476f7b6e6345d2b9af69e13995b4216c
satawatnack <60070093@kmitl.ac.th>
2019-11-27 20:46:43 +0700

    edit eureka

--- tokenByNodeJS/src/server.js
+++ tokenByNodeJS/src/server.js
@@ -1,40 +1,45 @@
-const express = require('express')
-require('./db/mongoose')
-const userRouter = require('./routers/user')
+const express = require("express");
+require("./db/mongoose");
+const userRouter = require("./routers/user");
 
-const app = express()
-const port = process.env.port || 8081
+const app = express();
+const port = process.env.port || 8080;
 
-app.use(express.json())
-app.use(userRouter)
+app.use(express.json());
+app.use(userRouter);
 
 app.listen(port, () => {
-    console.log('Server is up on port ' + port)
-})
+  console.log("Server is up on port " + port);
+});
 
-const Eureka = require('eureka-client').Eureka;
+const Eureka = require("eureka-client").Eureka;
 
 const eureka = new Eureka({
-    instance: {
-        app: 'auth-service',
-      instanceId: 'auth-service',
-      hostName: 'https://authservice-259910.appspot.com',
-      ipAddr: '127.0.0.1',
-      port:  {
-          '$': 8081,
-          '@enabled': 'true',
-      },
-      vipAddress: 'auth-service',
-      statusPageUrl: 'https://authservice-259910.appspot.com',
-      dataCenterInfo:  {
-          '@class': 'com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo',
-          name: 'MyOwn',
-      },
-      registerWithEureka: true,
-      fetchRegistry: true
-    }
+  instance: {
+    app: "authservice",
+    instanceId: "authservice",
+    hostName: "localhost",
+    ipAddr: "127.0.0.1",
+    port: {
+      $: 8080,
+      "@enabled": "true"
+    },
+    vipAddress: "authservice",
+    statusPageUrl: "https://authservicesop-259906.appspot.com/",
+    dataCenterInfo: {
+      "@class": "com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo",
+      name: "MyOwn"
+    },
+    registerWithEureka: true,
+    fetchRegistry: true
+  },
+  eureka: {
+    host: "discovery-259910.appspot.com",
+    port: 80,
+    servicePath: "/eureka/apps/"
+  }
 });
-eureka.logger.level('debug');
-eureka.start(function(error){
-    console.log(error || 'complete');
+eureka.logger.level("debug");
+eureka.start(function(error) {
+  console.log(error || "complete");
 });
